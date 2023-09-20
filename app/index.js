445bfc058c26a6e10a36bd0b95a6352473ea5d3c
Kubilay Türe <kubilayture@gmail.com>
2023-09-20 21:55:45 +0300

    jobs-details-page

--- app/index.js
+++ app/index.js
@@ -1,20 +1,24 @@
-import { useState } from 'react'
-import { View, ScrollView, SafeAreaView } from 'react-native'
-import { Stack, useRouter } from 'expo-router'
+import { useState } from "react"
+import { SafeAreaView, ScrollView, View } from "react-native"
+import { Stack, useRouter } from "expo-router"
 
-import { COLORS, icons, images, SIZES } from '../constants'
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+import { COLORS, icons, images, SIZES } from "../constants"
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components"
 
 const Home = () => {
   const router = useRouter()
+  const [searchTerm, setSearchTerm] = useState("")
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
-          headerStyle: {
-            backgroundColor: COLORS.lightWhite
-          },
+          headerStyle: { backgroundColor: COLORS.lightWhite },
           headerShadowVisible: false,
           headerLeft: () => (
             <ScreenHeaderBtn iconUrl={icons.menu} dimension='60%' />
@@ -22,19 +26,27 @@ const Home = () => {
           headerRight: () => (
             <ScreenHeaderBtn iconUrl={images.profile} dimension='100%' />
           ),
-          headerTitle: ''
+          headerTitle: "",
         }}
       />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-      >
+
+      <ScrollView showsVerticalScrollIndicator={false}>
         <View
           style={{
             flex: 1,
-            padding: SIZES.medium
+            padding: SIZES.medium,
           }}
         >
-          <Welcome />
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`)
+              }
+            }}
+          />
+
           <Popularjobs />
           <Nearbyjobs />
         </View>
